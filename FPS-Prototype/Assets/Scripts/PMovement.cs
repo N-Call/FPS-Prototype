03c5a0d508b962539ec1692eba8dc48910edaf63
Gabriel Fretter <fretterg@gmail.com>
2025-05-07 21:52:57 -0400

    One with redoing movement.
    - Removed Acceleration (It's being done in Unity already)
    - Removed Friction (It's being done in Unity already)
    - Fixed a bug were releasing the input after a period of time would cause a delay on stopping movement.
    - Combine the input handling and movement functions into one, for simplicity and better organization.

--- FPS-Prototype/Assets/Scripts/PMovement.cs
+++ FPS-Prototype/Assets/Scripts/PMovement.cs
@@ -1,73 +1,168 @@
-using Unity.VisualScripting.FullSerializer;
+//using UnityEngine;
+
+//public class PMovement : MonoBehaviour
+//{
+//    [SerializeField] CharacterController controller;
+//    [SerializeField] LayerMask ignoreLayer;
+
+//    [SerializeField] float speed;
+//    [SerializeField] float sprintMod;
+//    [SerializeField] int jumpMax;
+//    [SerializeField] float jumpForce;
+//    [SerializeField] float gravity;
+
+//    [SerializeField] int shootDamage;
+//    [SerializeField] float shootRate;
+//    [SerializeField] int shootDist;
+
+//    float shootTimer;
+
+//    int jumpCount;
+
+//    Vector3 moveDir;
+//    Vector3 playerVel; // This is for Y Velocity only!
+
+//    bool isSprinting;
+
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+
+//        movement();
+//        sprint();
+//    }
+//    void movement()
+//    {
+//        shootTimer += Time.deltaTime;
+
+//        if (controller.isGrounded)
+//        {
+//            jumpCount = 0;
+//            playerVel = Vector3.zero;
+//        }
+
+//        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
+
+//        //transform.position += moveDir * speed * Time.deltaTime;
+
+//        controller.Move(moveDir * speed * Time.deltaTime);
+
+//        jump();
+
+//        controller.Move(playerVel * Time.deltaTime);
+//        playerVel.y -= gravity * Time.deltaTime;
+
+//        if (Input.GetButton("Fire1") && shootTimer > shootRate)
+//            shoot();
+//    }
+//    void sprint()
+//    {
+//        if (Input.GetButtonDown("Sprint"))
+//        {
+//            speed *= sprintMod;
+//            isSprinting = true;
+//        }
+//        else if (Input.GetButtonUp("Sprint"))
+//        {
+//            speed /= sprintMod;
+//            isSprinting = false;
+//        }
+//    }
+
+//    void jump()
+//    {
+//        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+//        {
+//            jumpCount++;
+//            playerVel.y = jumpForce;
+
+//        }
+//    }
+
+//    void shoot()
+//    {
+//        shootTimer = 0;
+
+//        RaycastHit hit;
+//        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+//        {
+//            Debug.Log(hit.collider.name);
+//            IDamage dmg = hit.collider.GetComponent<IDamage>();
+
+//            if (dmg != null)
+//            {
+//                dmg.takeDamage(shootDamage);
+//            }
+//        }
+//    }
+//}
+
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
-    [SerializeField] private float walkAccel = 5f;
-    [SerializeField] private float walkSpeedMax = 10f;
-    [SerializeField] private float groundFriction = 0.25f;
+    [SerializeField] LayerMask ignoreLayer;
+
+    // Implement the speed with the sprinting modifier.
+    [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private float modSprint = 1.5f;
+
     [SerializeField] private float gravity = 9.81f;
-    [SerializeField] private float gravityMax = 36f;
+    [SerializeField] private float gravityMax = 10f;
 
     // This is used to determine how much upward force your character jumps.
-    [SerializeField] private float jumpForce = 16f;
+    [SerializeField] private float jumpForce = 8f;
     [SerializeField] private int maxJumps = 1;
 
-    private Vector3 currVel;
+    private Vector3 moveDir;
     private Vector3 inputDir;
-
     private Vector3 vertVel;
-
     private int currJumpCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // This checks the ipnut of the direction being pressed in question.
-        inputHandle();
-
-        // This applies movement after checking everything within the function.
-        moveApply();
+        // This checks the input of the direction being pressed in question, as well as overall movement.
+        handleMovement();
     }
 
-    void inputHandle()
+    void handleMovement()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         // Find the input direction in world space.
-        inputDir = (transform.right * h + transform.forward * v).normalized;
-    }
+        Vector3 inputDir = (transform.right * h + transform.forward * v);
 
-    void moveApply()
-    {
+        // This checks the sprinting on whether the 
+        bool isSprinting = Input.GetButton("Sprint");
+        float currSpeed = baseSpeed * (isSprinting ? modSprint : 1f);
+
+        moveDir = inputDir * baseSpeed;
+
         if (controller.isGrounded)
         {
             // This is done, so once you touch the ground, your jumps count resets.
             currJumpCount = 0;
             vertVel = Vector3.zero;
 
-            // This is needed to not just keep the player grounded
+            // This is needed to keep the player grounded.
             vertVel.y = -1f;
-
-            if (inputDir.magnitude > 0)
-            {
-                // Accelerate towards the direction of the input in question.
-                // Grabs the Max Walk Speed, if not at that speed, then grab the walk acceleration value.
-                currVel = Vector3.MoveTowards(currVel, inputDir * walkSpeedMax, walkAccel * Time.deltaTime);
-            }
-            else
-            {
-                // Appplies friction, if and only if there's no input involved.
-                currVel = Vector3.MoveTowards(currVel, Vector3.zero, groundFriction * Time.deltaTime);
-            }
 
             // This checks the input for jumping.
             if (Input.GetButtonDown("Jump"))
@@ -75,7 +170,6 @@ public class PMovement : MonoBehaviour
                 vertVel.y = jumpForce;
                 currJumpCount++;
             }
-
         }
         else
         {
@@ -94,8 +188,11 @@ public class PMovement : MonoBehaviour
             vertVel.y = Mathf.Max(vertVel.y, -gravityMax);
         }
 
+        // Apply the sprint modifier.
+        float targetspeed = baseSpeed * (Input.GetButton("Sprint") ? modSprint : 1f);
+
         // This grabs the H-movement and Fall speed.
-        Vector3 moveFinal = currVel + vertVel;
+        Vector3 moveFinal = moveDir + vertVel;
 
         // Now move the player using the controller itself after all of that is said and done.
         controller.Move(moveFinal * Time.deltaTime);
