d4f0c2bb452f0200fff339963604e1d7f0e348e5
Josh-Maddox <jjmaddox@student.fullsail.edu>
2025-05-08 18:27:42 -0700

    State pause made
    state pause function made

--- FPS-Prototype/Assets/Scripts/UI/GameManager.cs
+++ FPS-Prototype/Assets/Scripts/UI/GameManager.cs
@@ -19,12 +19,36 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PMovement>();
+        timeScaleOrig = Time.timeScale;
         
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Cancel"))
+        {
+            menuPause.SetActive(isPaused);
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
