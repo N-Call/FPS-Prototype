d0bfd368bd7d7e37b4931a857e276ddc56f3ad33
Josh-Maddox <jjmaddox@student.fullsail.edu>
2025-05-08 17:52:27 -0700

    Start of Game manager
    the very start of pause menu started and game manager script started

---
+++ FPS-Prototype/Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public GameObject player;
+    public PMovement playerScript;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
