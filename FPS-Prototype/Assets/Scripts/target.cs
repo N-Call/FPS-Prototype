972e8511e6446938765f17ee370a6855d47cc064
Symon Socha <simonraysocha@gmail.com>
2025-05-09 21:33:23 -0700

    Copied moving platform script to targets

--- FPS-Prototype/Assets/Scripts/target.cs
+++ FPS-Prototype/Assets/Scripts/target.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Internal;
 using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
@@ -5,33 +6,43 @@ using UnityEngine;
 
 public class Target : MonoBehaviour, IDamage, ITarget
 {
-
-    //[SerializeField] Renderer model;
-
     [SerializeField] int HP;
 
+    [Header("Direction")]
+    [SerializeField] Vector3 destination;
+    [SerializeField] bool relative;
 
-    //Ping Pong variables
-    Vector3 startPos;
-    public bool movingTarget;
-    public bool horizontal;
-    public bool vertical;
-    [SerializeField] int speed;
-    [SerializeField] float travelDistance;
+    [Header("Movement")]
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] bool lerp;
 
-    [SerializeField] private GameObject artToDisable = null;
+    [Header("Behavior")]
+    [SerializeField] float startDelay;
+    [SerializeField] float destinationDelay;
+    [SerializeField] bool pingPong;
+
+    Vector3 startPosition;
+    Vector3 dest;
+
+    float elapsedTime;
+
+    bool toStart;
+    bool waited;
+    bool finished;
+
     private Collider targCollider;
+    [SerializeField] private GameObject artToDisable = null;
 
     [Header("1: Speed. 2: Jump. 3: Time")]
     [SerializeField][Range(1, 3)] int element;
     bool affected;
 
     [Header("Speed Element")]
-    [SerializeField][Range(0.01f, 999999)] int speedMod;
+    [SerializeField][Range(0.01f, 999999)] float speedMod;
     [SerializeField] float speedModTime;
 
     [Header("Jump Element")]
-    [SerializeField][Range(0.01f, 999999)] int jumpMod;
+    [SerializeField][Range(0.01f, 999999)] float jumpMod;
     [SerializeField] float jumpModTime;
 
     [Header("Time Element")]
@@ -42,7 +53,9 @@ public class Target : MonoBehaviour, IDamage, ITarget
     void Start()
     {
         //colorOrig = model.material.color;
-        startPos = transform.position;
+        startPosition = transform.position;
+        dest = relative ? startPosition + destination : destination;
+        //startPos = transform.position;
         targCollider = GetComponent<Collider>();
 
     }
@@ -55,16 +68,40 @@ public class Target : MonoBehaviour, IDamage, ITarget
 
     void Movement()
     {
-        if (movingTarget)
+        // If the object has finished its movement
+        if (finished)
         {
-            if (horizontal)
+            return;
+        }
+
+        // Count up elapsed time
+        elapsedTime += Time.deltaTime;
+
+        // Check if they need to wait at start or destination
+        if (!waited && !Waited())
+        {
+            return;
+        }
+
+        // Handle movement
+        if (Move(transform.position, dest))
+        {
+            // If this object does not ping pong
+            // (does not move back and forth between start and destination),
+            // then the object has finished moving, and no longer needs to do anything
+            if (!pingPong)
             {
-                transform.position = startPos + transform.right * Mathf.PingPong(Time.time * speed, travelDistance);
+                finished = true;
+                return;
             }
-            if (vertical)
-            {
-                transform.position = startPos + transform.forward * Mathf.PingPong(Time.time * speed, travelDistance);
-            }
+
+            // Swap start & destination to move back and forth
+            Swap(ref startPosition, ref dest);
+
+            // Reset waiting and elapsed time
+            toStart = !toStart;
+            waited = false;
+            elapsedTime = 0.0f;
         }
     }
 
@@ -203,5 +240,47 @@ public class Target : MonoBehaviour, IDamage, ITarget
         yield return new WaitForSeconds(timeModTime);
 
         Time.timeScale = GameManager.instance.timeScaleOrig;
+    }
+
+    bool Waited()
+    {
+        if (!toStart && elapsedTime < startDelay)
+        {
+            return false;
+        }
+
+        if (toStart && elapsedTime < destinationDelay)
+        {
+            return false;
+        }
+
+        waited = true;
+        return true;
+    }
+
+    bool Move(Vector3 from, Vector3 to)
+    {
+        if (Vector3.Distance(from, to) <= 0.1f)
+        {
+            return true;
+        }
+
+        if (lerp)
+        {
+            transform.position = Vector3.Lerp(from, to, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position += (to - from).normalized * speed * Time.deltaTime;
+        }
+
+        return false;
+    }
+
+    void Swap(ref Vector3 one, ref Vector3 two)
+    {
+        Vector3 temp = one;
+        one = two;
+        two = temp;
     }
 }
