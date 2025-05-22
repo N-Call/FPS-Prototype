54c49e5b447f0a2bee374e22829f9023866b8d7d
Nicholas Call <210596407+N-Call@users.noreply.github.com>
2025-05-22 15:11:10 -0400

    refactor: renamed old spawner script

--- FPS-Prototype/Assets/Scripts/Level/MovingObjectSpawner.cs
+++ FPS-Prototype/Assets/Scripts/Level/MovingObjectSpawner.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class MovingObjectSpawner : MonoBehaviour
 {
 
     [Header("Spawner Settings")]
