e2221d00d50d7f6e1b1564457b16cd1f62f77807
Nicholas Call <210596407+N-Call@users.noreply.github.com>
2025-05-22 17:27:05 -0400

    feat: spawner script

---
+++ FPS-Prototype/Assets/Scripts/Level/Spawner.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    [Header("Base Settings")]
+    [SerializeField][Tooltip("The object to spawn")]
+    GameObject objectToSpawn;
+
+    [SerializeField][Tooltip("The positions the object can spawn at")]
+    protected Transform[] spawnPositions;
+
+    [Header("Spawn Settings")]
+    [SerializeField][Tooltip("The amount of objects to spawn")]
+    int spawnAmount;
+    
+    [SerializeField][Tooltip("The delay before the first object spawns when the player is in the trigger")]
+    float spawnDelay;
+
+    [SerializeField][Tooltip("The rate at which objects spawn")]
+    float spawnRate;
+
+    [Header("Enemy Specific Spawn Settings")]
+    [SerializeField][Tooltip("Add to enemy counter?")]
+    bool addToEnemyCounter = true;
+
+    [SerializeField]
+    [Tooltip("Add enemies to enemy counter at the start, or when they spawn in? False adds at start.")]
+    bool addWhenSpawned;
+
+    int currentIndex;
+    int amountSpawned;
+
+    float spawnDelayTimer;
+    float spawnTimer;
+
+    bool isEnemy;
+    bool firstSpawned;
+    bool hasPlayer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isEnemy = objectToSpawn.CompareTag("Enemy");
+        if (isEnemy && addToEnemyCounter && !addWhenSpawned)
+        {
+            GameManager.instance.UpdateEnemyCounter(amountSpawned);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasPlayer || objectToSpawn == null || spawnPositions.Length == 0 || amountSpawned >= spawnAmount)
+        {
+            return;
+        }
+
+        if (!firstSpawned)
+        {
+            spawnTimer = spawnRate;
+        }
+
+        if (spawnDelayTimer < spawnDelay)
+        {
+            spawnDelayTimer += Time.deltaTime;
+            return;
+        }
+
+        if (spawnTimer < spawnRate)
+        {
+            spawnTimer += Time.deltaTime;
+            return;
+        }
+
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        firstSpawned = true;
+        
+        int index = GetPositionIndex();
+        Instantiate(objectToSpawn, spawnPositions[index].position, spawnPositions[index].rotation);
+
+        if (isEnemy && addToEnemyCounter && addWhenSpawned)
+        {
+            GameManager.instance.UpdateEnemyCounter(1);
+        }
+
+        amountSpawned++;
+        spawnTimer = 0.0f;
+    }
+
+    protected virtual int GetPositionIndex()
+    {
+        int index = currentIndex;
+
+        if (index == spawnPositions.Length)
+        {
+            currentIndex = 1;
+            return 0;
+        }
+
+        currentIndex++;
+        return index;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasPlayer = false;
+        }
+    }
+
+}
