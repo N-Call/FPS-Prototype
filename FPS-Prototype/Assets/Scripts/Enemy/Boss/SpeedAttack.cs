ba832e6e71a94d9f9eb5496c078cd8c72d48d0b8
gkevin477 <Gkevin477@gmail.com>
2025-05-26 22:08:19 -0500

    boss Laser beam implemented
    boss Laser beam has been made

--- FPS-Prototype/Assets/Scripts/Enemy/Boss/SpeedAttack.cs
+++ FPS-Prototype/Assets/Scripts/Enemy/Boss/SpeedAttack.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class SpeedAttack : BaseState
 {
     private BossSM bossSM;
+    private float attackTimer;
+    private float speedTimer;
+    private bool isTransforming;
+    private int origSpeed;
     public SpeedAttack(StateMachine stm) : base(name: "Speeding", stm)
     {
         bossSM = (BossSM)this.stateMachine;
@@ -11,19 +15,50 @@ public class SpeedAttack : BaseState
     public override void Enter()
     {
         base.Enter();
-        bossSM.animator.CrossFade("Shoot", 0.2f);
+        bossSM.animator.CrossFade("SpeedTransform", 0.2f);
+        origSpeed = bossSM.speedAmount;
+        bossSM.agent.acceleration = bossSM.speedAmount;
+        bossSM.agent.isStopped = false;
     }
     public override void StateLogic()
     {
         base.StateLogic();
+        if (speedTimer >= bossSM.speedDuration && !isTransforming)
+        {
+            bossSM.animator.CrossFade("SpeedToNormal", 0.2f);
+            isTransforming = true;
+        }
     }
     public override void Action()
     {
         base.Action();
+        if(isTransforming) { return; }
+        speedTimer += Time.deltaTime;
+        bossSM.agent.SetDestination(GameManager.instance.player.transform.position);
+        bossSM.transform.LookAt(new Vector3(GameManager.instance.player.transform.position.x, bossSM.transform.position.y, GameManager.instance.player.transform.position.z));
+        if (bossSM.animator.GetCurrentAnimatorStateInfo(0).IsName("SpeedIdle"))
+        {
+            attackTimer += Time.deltaTime;
+        }
+
+        if (attackTimer >= bossSM.speedAttackRate && Vector3.Distance(GameManager.instance.player.transform.position, bossSM.transform.position) < bossSM.speedAttackRange)
+        {
+            bossSM.animator.CrossFade("SpeedAttack", 0.2f);
+
+            attackTimer = 0;
+        }
+    }
+
+    public void ChangeToIdle()
+    {
+        bossSM.ChangeState(bossSM.idle);
     }
 
     public override void Exit()
     {
         base.Exit();
+        bossSM.agent.isStopped = true;
+        bossSM.agent.acceleration = origSpeed;
+
     }
 }
