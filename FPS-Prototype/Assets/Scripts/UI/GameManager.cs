0a9460f0ff0ddf2568078f27e0bf3c98a9aeb8f8
Josh-Maddox <jjmaddox@student.fullsail.edu>
2025-06-13 13:40:44 -0700

    added more to the upgrade script
    pushing fix of next level btn

--- FPS-Prototype/Assets/Scripts/UI/GameManager.cs
+++ FPS-Prototype/Assets/Scripts/UI/GameManager.cs
@@ -93,9 +93,9 @@ public class GameManager : MonoBehaviour
     int enemyCount;
     int scrapCounter = 0;
     int totalScrap = 10000;//for testing only
-    //public List<UpgradeData> allUpgrades;
-    //int completed = 0;
-   // int total = 0;
+    public List<UpgradeData> allUpgrades;
+    int completed = 0;
+    int total = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -231,129 +231,129 @@ public class GameManager : MonoBehaviour
         totalScrap += amount;
         scrapUI.text = scrapCounter.ToString("F0");
     }
-    //public bool SpendScrap(int amount)
-    //{
-    //    if (totalScrap >= amount)
-    //    {
-    //        totalScrap -= amount;
-    //        Debug.Log(totalScrap + "My Money");
-    //        totalScrapUI.text = totalScrap.ToString("F0");
-            
-    //        return true;
-    //    }
-    //    return false;
-    //}
+    public bool SpendScrap(int amount)
+    {
+        if (totalScrap >= amount)
+        {
+            totalScrap -= amount;
+            Debug.Log(totalScrap + "My Money");
+            totalScrapUI.text = totalScrap.ToString("F0");
+
+            return true;
+        }
+        return false;
+    }
 
     public void ShowScrap()
     {
         totalScrapUI.text = totalScrap.ToString("F0");
     }
 
-    //public bool CanBuy(UpgradeData upgrade)
-    //{
-    //    if (upgrade.isMajor)
-    //    {
-    //        Debug.Log("Can I buy major");
-    //        return CanBuyMajor(upgrade);
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Maxed out lvl need to prompt player");
-    //        return upgrade.currentLevel < upgrade.maxLevel &&
-    //               totalScrap >= upgrade.costPerLevel[upgrade.currentLevel];
-    //    }
-    //}
-    //private bool CanBuyMajor(UpgradeData upgrade)
-    //{
+    public bool CanBuy(UpgradeData upgrade)
+    {
+        if (upgrade.isMajor)
+        {
+            Debug.Log("Can I buy major");
+            return CanBuyMajor(upgrade);
+        }
+        else
+        {
+            Debug.Log("Maxed out lvl need to prompt player");
+            return upgrade.currentLevel < upgrade.maxLevel &&
+                   totalScrap >= upgrade.costPerLevel[upgrade.currentLevel];
+        }
+    }
+    private bool CanBuyMajor(UpgradeData upgrade)
+    {
 
-    //    // Check if it's a weapon upgrade
-    //    bool isWeapon = upgrade.category.ToString().Contains("Weapon 1");
-    //    bool isWeapon2 = upgrade.category.ToString().Contains("Weapon 2");
-    //    bool isWeapon3 = upgrade.category.ToString().Contains("Weapon 3");
+        // Check if it's a weapon upgrade
+        bool isWeapon = upgrade.category.ToString().Contains("Weapon 1");
+        bool isWeapon2 = upgrade.category.ToString().Contains("Weapon 2");
+        bool isWeapon3 = upgrade.category.ToString().Contains("Weapon 3");
 
-    //    foreach (UpgradeData up in allUpgrades)
-    //    {
-    //        // For weapons, count all non-major weapon upgrades (across all weapon categories)
-    //        if (isWeapon && !up.isMajor)
-    //        {
-    //            total++;
-    //            if (up.currentLevel == up.maxLevel)
-    //            {
-    //                completed++;
-    //            }
-    //        }
-    //        else if(isWeapon2 && !up.isMajor)
-    //        {
-    //            total++;
-    //            if (up.currentLevel == up.maxLevel)
-    //            {
-    //                completed++;
-    //            }
-    //        }
-    //        else if (isWeapon3 && !up.isMajor)
-    //        {
-    //            total++;
-    //            if (up.currentLevel == up.maxLevel)
-    //            {
-    //                completed++;
-    //            }
-    //        }
+        foreach (UpgradeData up in allUpgrades)
+        {
+            // For weapons, count all non-major weapon upgrades (across all weapon categories)
+            if (isWeapon && !up.isMajor)
+            {
+                total++;
+                if (up.currentLevel == up.maxLevel)
+                {
+                    completed++;
+                }
+            }
+            else if (isWeapon2 && !up.isMajor)
+            {
+                total++;
+                if (up.currentLevel == up.maxLevel)
+                {
+                    completed++;
+                }
+            }
+            else if (isWeapon3 && !up.isMajor)
+            {
+                total++;
+                if (up.currentLevel == up.maxLevel)
+                {
+                    completed++;
+                }
+            }
 
-    //        // For movement or orbs, count only same-category minor upgrades
-    //        else if (!isWeapon && up.category == upgrade.category && !up.isMajor)
-    //        {
-    //            total++;
-    //            if (up.currentLevel == up.maxLevel)
-    //            {
-    //                completed++;
-    //            }
-    //        }
-    //    }
-    //    // weapons : unlock after 10 of 15
-    //    if (isWeapon)
-    //    {
-    //        Debug.Log("Checking for weapon");
-    //        Debug.Log(completed);
-    //        return completed >= 10 && totalScrap >= upgrade.majorCost;
-           
-    //    }
-    //    // movement /orbs unlock after all upgrades
-    //    return completed == total && totalScrap > +upgrade.majorCost;
-    //}
+            // For movement or orbs, count only same-category minor upgrades
+            else if (!isWeapon && up.category == upgrade.category && !up.isMajor)
+            {
+                total++;
+                if (up.currentLevel == up.maxLevel)
+                {
+                    completed++;
+                }
+            }
+        }
+        // weapons : unlock after 10 of 15
+        if (isWeapon)
+        {
+            Debug.Log("Checking for weapon");
+            Debug.Log(completed);
+            return completed >= 10 && totalScrap >= upgrade.majorCost;
 
-    //public void BuyUpgrade(UpgradeData upgrade)
-    //{
-    //    if (!CanBuy(upgrade)) return;
+        }
+        // movement /orbs unlock after all upgrades
+        return completed == total && totalScrap > +upgrade.majorCost;
+    }
 
-    //    if(upgrade.isMajor && upgrade.currentLevel < upgrade.maxLevel)
-    //    {
-    //        Debug.Log("I bought a major");
-    //        SpendScrap(upgrade.majorCost);
-    //        upgrade.currentLevel++;
-    //        ApplyMajorUpgrade(upgrade);
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("I bought minors");
-    //        int cost = upgrade.costPerLevel[upgrade.currentLevel];
-    //        SpendScrap(cost);
-    //        upgrade.currentLevel++;
-    //        ApplyMinorUpgrade(upgrade);
-    //    }
-    //}
+    public void BuyUpgrade(UpgradeData upgrade)
+    {
+        if (!CanBuy(upgrade)) return;
 
-    //private void ApplyMinorUpgrade(UpgradeData upgrade)
-    //{
-    //    Debug.Log("Minor upgrade applied: " + upgrade.name + " to level "+ upgrade.currentLevel);
-    //    // Apply minor upgrade effect here
+        if (upgrade.isMajor && upgrade.currentLevel < upgrade.maxLevel)
+        {
+            Debug.Log("I bought a major");
+            SpendScrap(upgrade.majorCost);
+            upgrade.currentLevel++;
+            ApplyMajorUpgrade(upgrade);
+        }
+        else
+        {
+            Debug.Log("I bought minors");
+            int cost = upgrade.costPerLevel[upgrade.currentLevel];
+            SpendScrap(cost);
+            upgrade.currentLevel++;
+            ApplyMinorUpgrade(upgrade);
+        }
+    }
 
-    //}
+    private void ApplyMinorUpgrade(UpgradeData upgrade)
+    {
+        Debug.Log("Minor upgrade applied: " + upgrade.name + " to level " + upgrade.currentLevel);
+        // Apply minor upgrade effect here
 
-    //private void ApplyMajorUpgrade(UpgradeData upgrade)
-    //{
-    //    Debug.Log("Major upgrade unlocked: " + upgrade.name);
-    //    // Apply major upgrade effect here
-    //}
+    }
+
+    private void ApplyMajorUpgrade(UpgradeData upgrade)
+    {
+        Debug.Log("Major upgrade unlocked: " + upgrade.name);
+        // Apply major upgrade effect here
+    }
 
     public void NextLvlBtnOff()
     {
