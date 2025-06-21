8c1055e2450bbf880b6be10fc599645908930fce
Josh-Maddox <jjmaddox@student.fullsail.edu>
2025-06-20 17:49:10 -0700

    change UI back to OG

--- FPS-Prototype/Assets/Scripts/UI/UpgradeShop/ImageHoverEffect.cs
+++ FPS-Prototype/Assets/Scripts/UI/UpgradeShop/ImageHoverEffect.cs
@@ -1,8 +1,7 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
-using System.Reflection;
+using UnityEngine;
 
 public class ImageHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
@@ -10,75 +9,29 @@ public class ImageHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExi
     public Image initialImage;
 
     public UpgradeData upgradeData;
-    public TMP_Text upText;
 
-    void Start()
-    {
-        LoadUpgradeLevel();
-    }
+    public TMP_Text upText;
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        Debug.Log("point is down");
         GameManager.instance.BuyUpgrade(upgradeData);
-
         if (upText != null)
+        {// need to add UI count to major Upgrades
             upText.text = upgradeData.currentLevel.ToString();
-
-        SaveUpgradeLevel();
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (hoverImage != null)
-            hoverImage.gameObject.SetActive(true);
+        {
+            hoverImage.gameObject.SetActive(true); // Show hover image
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (hoverImage != null)
-            hoverImage.gameObject.SetActive(false);
-    }
-
-    private void LoadUpgradeLevel()
-    {
-        if (upgradeData == null || string.IsNullOrEmpty(upgradeData.upgradeID)) return;
-
-        var abilities = GameManager.instance.playerAbilities;
-        var field = typeof(PlayerAbilities).GetField(upgradeData.upgradeID);
-
-        if (field != null && field.FieldType == typeof(int))
-        {
-            upgradeData.currentLevel = (int)field.GetValue(abilities);
-            if (upText != null)
-                upText.text = upgradeData.currentLevel.ToString();
-
-            Debug.Log($"[Load] {upgradeData.upgradeID} = {upgradeData.currentLevel}");
-        }
-    }
-
-    private void SaveUpgradeLevel()
-    {
-        if (upgradeData == null || string.IsNullOrEmpty(upgradeData.upgradeID)) return;
-
-        var abilities = GameManager.instance.playerAbilities;
-        var field = typeof(PlayerAbilities).GetField(upgradeData.upgradeID);
-
-        if (field != null && field.FieldType == typeof(int))
-        {
-            field.SetValue(abilities, upgradeData.currentLevel);
-            Debug.Log($"[Save] {upgradeData.upgradeID} = {upgradeData.currentLevel}");
-        }
-
-        // Save major upgrade flag if max level is reached
-        if (!string.IsNullOrEmpty(upgradeData.majorUpgradeID) && upgradeData.currentLevel >= upgradeData.maxLevel)
-        {
-            var majorField = typeof(PlayerAbilities).GetField(upgradeData.majorUpgradeID);
-
-            if (majorField != null && majorField.FieldType == typeof(bool))
-            {
-                majorField.SetValue(abilities, true);
-                Debug.Log($"[Major Save] {upgradeData.majorUpgradeID} = true");
-            }
-        }
+        hoverImage.gameObject.SetActive(false); // Hide hover image
     }
 }
