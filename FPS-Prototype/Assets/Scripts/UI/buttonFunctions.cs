0f7f21b46f71ac81c81c038682480d58b5d93904
Josh-Maddox <jjmaddox@student.fullsail.edu>
2025-06-22 19:35:03 -0700

    added buildindex to 14

--- FPS-Prototype/Assets/Scripts/UI/buttonFunctions.cs
+++ FPS-Prototype/Assets/Scripts/UI/buttonFunctions.cs
@@ -80,17 +80,12 @@ public class ButtonFunctions : MonoBehaviour
     public void NextLevel()
     {
         // this is to load the next level but does a check first on making sure your in scene count 
-        if (SceneManager.GetActiveScene().buildIndex + 1 <= 13)
+        if (SceneManager.GetActiveScene().buildIndex + 1 <= 14)
         {
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             SoundManager.instance.sfxSource.Stop();
             GameManager.instance.StateUnpause();
-        }
-        else if (SceneManager.GetActiveScene().buildIndex == 13)
-        {
-            // only works after the button is clicked if prefab is updated of UI then go to last lvl turn off next button and save scene
-            GameManager.instance.NextLvlBtnOff();
         }
     }
     public void LvlOverWorldSave()
